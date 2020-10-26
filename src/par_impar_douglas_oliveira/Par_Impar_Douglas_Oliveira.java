beb275b32463926d518a68f9d0bc4981eba738a0
DouglasElloa <73400686+DouglasElloa@users.noreply.github.com>
2020-10-26 19:41:54 -0300

    Criando Projeto PAr e Impar

---
+++ src/par_impar_douglas_oliveira/Par_Impar_Douglas_Oliveira.java
@@ -0,0 +1,29 @@
+
+package par_impar_douglas_oliveira;
+
+import java.util.Scanner;
+
+public class Par_Impar_Douglas_Oliveira {
+
+    
+    
+    public static void main(String[] args) 
+    { Scanner entrada;
+      int num1;
+      entrada = new Scanner (System.in);
+      System.out.println("digite um número");
+      num1 = entrada.nextInt();
+      if(num1 % 2 == 0)
+      {System.out.println("O número:" +num1+ " é Par");
+      
+    }else  {System.out.println("O número:" +num1+ " é Ímpar");
+      }
+    
+    
+    
+            
+    
+        
+    }
+    
+}
